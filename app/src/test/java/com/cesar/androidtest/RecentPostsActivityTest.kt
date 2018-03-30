272f588f4a25a55fffa6041758c8c174e83cfd2c
Cesar Castro <cesar-web@bol.com.br>
2018-03-30 16:12:47 -0300

    Build first test for RecentPostsActivity

--- app/src/test/java/com/cesar/androidtest/RecentPostsActivityTest.kt
+++ app/src/test/java/com/cesar/androidtest/RecentPostsActivityTest.kt
@@ -1,17 +1,32 @@
 package com.cesar.androidtest
 
+import com.cesar.androidtest.recentposts.RecentPostsActivity
+import com.cesar.androidtest.recentposts.di.RecentPostsTestActivityMock
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class RecentPostsActivityTest {
+
+    private lateinit var activity : RecentPostsActivity
+
+    @Before
+    fun setUp() {
+        activity = Robolectric.buildActivity(RecentPostsTestActivityMock::class.java).create().get()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun whenLoadActivityThenRequestPostsList() {
+        verify(activity.recentPostsPresenter, times(1)).onLoad()
     }
 }
