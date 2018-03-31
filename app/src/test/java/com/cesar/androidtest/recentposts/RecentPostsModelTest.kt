3ff997cfcd6013d2069652054b9d8d98868beadc
Cesar Castro <cesar-web@bol.com.br>
2018-03-31 18:59:50 -0300

    Add API response test to RecentPostsModelTest

--- app/src/test/java/com/cesar/androidtest/recentposts/RecentPostsModelTest.kt
+++ app/src/test/java/com/cesar/androidtest/recentposts/RecentPostsModelTest.kt
@@ -1,17 +1,17 @@
 package com.cesar.androidtest.recentposts
 
+import com.cesar.androidtest.recentposts.model.RecentPostModel
 import com.cesar.androidtest.recentposts.model.RecentPostsApi
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.*
 import org.mockito.Mockito.*
-import org.mockito.MockitoAnnotations
 
 class RecentPostsModelTest {
 
     // Helper function for Mockito with Kotlin
     private fun <T> any(): T {Mockito.any<T>();return uninitialized()}
+    @Suppress("UNCHECKED_CAST")
     private fun <T> uninitialized(): T = null as T
 
     private lateinit var model: RecentPostsModel
@@ -19,6 +19,8 @@ class RecentPostsModelTest {
     private lateinit var mockApi: RecentPostsApi
     @Mock
     private lateinit var mockPresenter: RecentPostsPresenter
+    @Captor
+    private lateinit var resultListenerArgumentCaptor: ArgumentCaptor<RecentPostsApi.ResultListener>
 
     @Before
     fun setUp() {
@@ -30,7 +32,18 @@ class RecentPostsModelTest {
     @Test
     fun whenRequestListThenCallApiList() {
         model.requestList()
-        verify(mockApi, times(1)).list(callback = any())
+        verify(mockApi).list(callback = any())
+    }
+
+    @Test
+    fun givenListRequestedWhenResponseSuccessfulThenCallPresenterRequestListResponseSuccessful() {
+        val anyList = listOf(RecentPostModel())
+        model.requestList()
+
+        verify(mockApi).list(resultListenerArgumentCaptor.capture())
+        resultListenerArgumentCaptor.value.onResponseSuccessful(anyList)
+
+        verify(mockPresenter).onRequestListResponseSuccessful(anyList)
     }
 
 }
