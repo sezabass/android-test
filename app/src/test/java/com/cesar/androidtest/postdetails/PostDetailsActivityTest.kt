8ad6a94cf4997b8a9cbf8a9c11864a2673ddaedc
Cesar Castro <cesar-web@bol.com.br>
2018-04-02 04:07:20 -0300

    Implement PostDetails tests DI for Activity testing

--- app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsActivityTest.kt
+++ app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsActivityTest.kt
@@ -1,10 +1,37 @@
 package com.cesar.androidtest.postdetails
 
+import android.content.Intent
+import com.cesar.androidtest.BuildConfig
+import com.cesar.androidtest.postdetails.di.PostDetailsTestActivityMock
+import com.cesar.androidtest.recentposts.RecentPostsActivity
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = [27])
 class PostDetailsActivityTest {
+    private lateinit var activity: PostDetailsActivity
+
     @Before
     fun setUp() {
+        val intent = Intent(RuntimeEnvironment.application, PostDetailsTestActivityMock::class.java)
+        intent.putExtra(RecentPostsActivity.KEY_POST_ID, "samplePostId")
+        intent.putExtra(RecentPostsActivity.KEY_POST_TITLE, "samplePostTitle")
+
+        activity = Robolectric.buildActivity(PostDetailsTestActivityMock::class.java, intent).create().get()
+    }
+
+    @Test
+    fun whenLoadActivityThenRequestCommentsList() {
+        Mockito.verify(activity.presenter).onLoad(anyString())
     }
 
 }
