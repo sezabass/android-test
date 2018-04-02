209238cbf7b170714e89aaa8caaefc04447c3133
Cesar Castro <cesar-web@bol.com.br>
2018-04-02 03:25:40 -0300

    Add PostDetails model and presenter tests

--- app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsModelTest.kt
+++ app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsModelTest.kt
@@ -1,10 +1,64 @@
 package com.cesar.androidtest.postdetails
 
+import com.cesar.androidtest.postdetails.model.Comment
+import com.cesar.androidtest.postdetails.model.PostDetailsApi
+import com.nhaarman.mockito_kotlin.same
 import org.junit.Before
+import org.junit.Test
+import org.mockito.*
+import org.mockito.Mockito.verify
 
-class PostDetailsModelTest {
+class PostDetailsModelTest {// Helper function for Mockito with Kotlin
+private fun <T> any(): T {
+    Mockito.any<T>();return uninitialized()}
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> uninitialized(): T = null as T
+
+    private lateinit var model: PostDetailsModel
+    @Mock
+    private lateinit var mockApi: PostDetailsApi
+    @Mock
+    private lateinit var mockPresenter: PostDetailsContract.Presenter
+    @Captor
+    private lateinit var resultListenerArgumentCaptor: ArgumentCaptor<PostDetailsApi.ResultListener>
+
+    private val idSample: String = "idSample"
+
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        model = PostDetailsModel(mockApi)
+        model.presenter = mockPresenter
+    }
+
+
+    @Test
+    fun whenRequestListThenCallApiList() {
+        model.loadComments(idSample)
+        verify(mockApi).list(same(idSample), callback = any())
+    }
+
+    @Test
+    fun givenCommentsRequestedWhenResponseSuccessfulThenCallPresenterOnRequestListResponseSuccessful() {
+        val anyList = listOf(Comment())
+        model.loadComments(idSample)
+
+        verify(mockApi).list(same(idSample), resultListenerArgumentCaptor.capture())
+        resultListenerArgumentCaptor.value.onResponseSuccessful(anyList)
+
+        verify(mockPresenter).onRequestListResponseSuccessful(anyList)
+    }
+
+
+    @Test
+    fun givenCommentsRequestedWhenFailureThenCallPresenterOnRequestListFailure() {
+        val anyMessage = "Any message"
+        model.loadComments(idSample)
+
+        verify(mockApi).list(same(idSample), resultListenerArgumentCaptor.capture())
+        resultListenerArgumentCaptor.value.onFailure(anyMessage)
+
+        verify(mockPresenter).onRequestListFailure()
     }
 
 }
--- app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsPresenterTest.kt
+++ app/src/test/java/com/cesar/androidtest/postdetails/PostDetailsPresenterTest.kt
@@ -1,3 +1,53 @@
 package com.cesar.androidtest.postdetails
 
-class PostDetailsPresenterTest
+import com.cesar.androidtest.postdetails.model.Comment
+import com.nhaarman.mockito_kotlin.same
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.Mockito.anyList
+import org.mockito.Mockito.verify
+
+class PostDetailsPresenterTest {
+
+    // Helper function for Mockito with Kotlin
+    private fun <T> any(): T {
+        Mockito.any<T>();return uninitialized()}
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> uninitialized(): T = null as T
+
+    lateinit var mockView: PostDetailsContract.View
+    lateinit var mockModel: PostDetailsContract.Model
+    lateinit var presenter: PostDetailsContract.Presenter
+
+    private val lastItemSample: String = "lastItemSample"
+
+    @Before
+    fun setUp() {
+        mockView = Mockito.mock(PostDetailsContract.View::class.java)
+        mockModel = Mockito.mock(PostDetailsContract.Model::class.java)
+
+        presenter = PostDetailsPresenter(mockView, mockModel)
+    }
+
+    @Test
+    fun whenOnLoadThenCallModelLoadComments() {
+        val postId = "samplePostId"
+        presenter.onLoad(postId)
+        verify(mockModel).loadComments(same(postId))
+    }
+
+    @Test
+    fun onRequestListResponseSuccessful() {
+        val response: List<Comment> = listOf(Comment())
+        presenter.onRequestListResponseSuccessful(response)
+        verify(mockView).onLoadCommentsSuccess(anyList<Comment>())
+    }
+
+    @Test
+    fun onRequestListFailure() {
+        presenter.onRequestListFailure()
+        verify(mockView).onLoadCommentsFailure()
+    }
+}
